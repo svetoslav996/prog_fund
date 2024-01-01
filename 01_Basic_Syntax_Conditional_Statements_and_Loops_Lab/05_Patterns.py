a91144f41e1ef45f1c5883e60271665d8100ccdf
svetoslav996 <30300746+svetoslav996@users.noreply.github.com>
2024-01-01 23:00:34 +0200

    Add files via upload

---
+++ 01_Basic_Syntax_Conditional_Statements_and_Loops_Lab/05_Patterns.py
@@ -0,0 +1,11 @@
+number = int(input())
+
+for i in range(number):
+    for j in range(0, i+1):
+        print("*", end="")
+    print()
+
+for i in range(number - 1):
+    for j in range(number - 1, i, -1):
+        print("*", end="")
+    print()

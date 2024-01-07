e6e8ff607a8abc48dd2f048732ad323847bff908
svetoslav996 <30300746+svetoslav996@users.noreply.github.com>
2024-01-07 19:32:50 +0200

    Delete 03_Lists_Basics_Exam directory

--- 03_Lists_Basics_Exam/01_Invert_Values.py
+++
@@ -1,9 +0,0 @@
-nums = input().split(' ')
-new_list = []
-
-for num in nums:
-    if int(num) > 0:
-        new_list.append(-int(num))
-    else:
-        new_list.append(abs(int(num)))
-print(new_list)
